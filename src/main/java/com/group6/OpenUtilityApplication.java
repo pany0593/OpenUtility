cba5db7fec018274a163203e8ebfb9e92f1d513b
pany0593 <1569987504@qq.com>
2024-10-24 00:15:09 +0800

    first add

---
+++ src/main/java/com/group6/OpenUtilityApplication.java
@@ -0,0 +1,12 @@
+package com.group6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenUtilityApplication {
+    public static void main( String[] args )
+    {
+        SpringApplication.run(OpenUtilityApplication.class,args);
+    }
+}

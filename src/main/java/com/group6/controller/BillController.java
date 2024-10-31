5fcc104e72299dc6d689d7aaa3e489ae29763d49
pany0593 <1569987504@qq.com>
2024-10-31 19:06:36 +0800

    update: Base.java Result.java

--- src/main/java/com/group6/controller/BillController.java
+++ src/main/java/com/group6/controller/BillController.java
@@ -1,4 +1,48 @@
 package com.group6.controller;
 
+
+import com.group6.pojo.Bill;
+import com.group6.pojo.Result;
+import org.springframework.web.bind.annotation.*;
+
+import java.math.BigDecimal;
+
+@RestController
+@RequestMapping("/bill")
 public class BillController {
+    @PostMapping("/Add")
+    public Result Add(Bill bill) {
+
+    }
+
+    @PostMapping("/delete")
+    public Result delete(String id) {
+
+    }
+
+    @PostMapping("/upload")
+    public Result upload(Bill bill) {
+
+    }
+
+    @GetMapping("/getData")
+    public Result getData(String id) {
+
+    }
+
+    @GetMapping("/count_dormitory")
+    public Result count_dormitory(int id, int start_year, int start_month, int end_year, int end_month) {
+
+    }
+
+    @GetMapping("/count_building")
+    public Result count_building(int building, int start_year, int start_month, int end_year, int end_month) {
+
+    }
+
+    @GetMapping("/count_school")
+    public Result count_school(int start_year, int start_month, int end_year, int end_month) {
+
+    }
+
 }
--- src/main/java/com/group6/pojo/Bill.java
+++ src/main/java/com/group6/pojo/Bill.java
@@ -1,4 +1,19 @@
 package com.group6.pojo;
 
+import java.math.BigDecimal;
+
 public class Bill {
+
+    private int id;
+    private int year;
+    private int month;
+    private int days;
+    private int building;
+    private int dormitory;
+    private BigDecimal electricity_usage;
+    private BigDecimal electricity_cost;
+    private BigDecimal water_usage;
+    private BigDecimal water_cost;
+    private BigDecimal total_cost;
+
 }

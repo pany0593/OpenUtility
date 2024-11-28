dd7fe18c730bad1b5c446451c1de7bfc327412b4
pany0593 <1569987504@qq.com>
2024-11-28 18:22:53 +0800

    fix: SecurityConfig

--- src/main/java/com/group6/OpenUtilityApplication.java
+++ src/main/java/com/group6/OpenUtilityApplication.java
@@ -2,8 +2,9 @@ package com.group6;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class OpenUtilityApplication
 {
     public static void main(String[] args)
